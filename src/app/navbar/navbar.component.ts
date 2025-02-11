37638a198bbb1cb0f89dfc938c9116ca19f77cf3
gundlach <Veronika.Gundlach@adesso.de>
2025-02-11 13:48:10 +0100

    initial commit:
    angular base project

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matPersonOutline } from '@ng-icons/material-icons/outline'
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgIcon],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  viewProviders: [provideIcons({ matPersonOutline })]
+})
+export class NavbarComponent {
+
+  openOptions($event: MouseEvent) {
+    console.log("clicked Button");
+  }
+}
