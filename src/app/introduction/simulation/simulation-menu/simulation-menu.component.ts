73dad26f556cf21053b9c30ed2a3e9e605cf92b4
veroolovegood <veronah@gmx.de>
2025-04-03 16:25:11 +0200

    finished simulation navigation

--- src/app/introduction/simulation/simulation-menu/simulation-menu.component.ts
+++ src/app/introduction/simulation/simulation-menu/simulation-menu.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matInfoOutline } from '@ng-icons/material-icons/outline';
 
 @Component({
   selector: 'app-simulation-menu',
-  imports: [],
+  imports: [
+    NgIcon
+  ],
+  viewProviders: [provideIcons({matInfoOutline})],
   templateUrl: './simulation-menu.component.html',
   styleUrl: './simulation-menu.component.scss'
 })
 export class SimulationMenuComponent {
 
+  constructor(private router: Router) {
+  }
+
+  navigateTo(section: string) {
+    this.router.navigate([section]);
+  }
 }
