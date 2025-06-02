fcebcc3403177d54eafb0111be282767f325ff8f
veroolovegood <veronah@gmx.de>
2025-06-02 21:27:10 +0200

    made code editor more reusable

--- src/app/common/code-editor/code-editor.component.ts
+++ src/app/common/code-editor/code-editor.component.ts
@@ -1,53 +1,43 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, output, ViewChild } from '@angular/core';
 import { basicSetup, EditorView } from 'codemirror';
 import { css } from '@codemirror/lang-css';
 import { HighlightStyle, syntaxHighlighting } from '@codemirror/language';
 import { tags } from '@lezer/highlight';
 import { WebshopCodeService } from '../../usecases/webshop/service/webshop-code.service';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-code-editor',
-  imports: [
-    NgIf
-  ],
   templateUrl: './code-editor.component.html',
   styleUrl: './code-editor.component.scss',
 })
-export class CodeEditorComponent implements AfterViewInit {
+export class CodeEditorComponent implements AfterViewInit, OnInit {
+
+  checkAnswer = output<Text>();
+
   @ViewChild('editor') editor: any;
 
-  explanationText: string = 'In dieser Lektion schauen wir uns den Stellenwert der Schriftgröße an. Auf der ' +
-    'Webseite ist der aus der Einführungslektion bekannte Webshop dargestellt, wie eine Person mit starker ' +
-    'Weitsichtigkeit ihn wahrnehmen würde. Die spezielle visuelle Einschränkung kannst du über den Dropdown-Button ' +
-    'anpassen. \n' +
-    'Versuch nun zunächst den hinzugefügten Text zu lesen. Ganz schön schwierig oder? Wie sieht es mit vergrößern aus ' +
-    '(Die Textgröße kannst du über den zusätzlichen Input verändern)? Die Webseite wurde so gebaut, dass automatisches ' +
-    'Vergrößern keine Auswirkungen auf die Schriftgröße hat. Das ist natürlich schlecht für Personen, die Schwierigkeiten haben, kleinere Texte wahrzunehmen.\n' +
-    '\n' +
-    'Versuch die CSS-Klasse so anzupassen, dass der Text beim Vergrößern lesbar ist und auch alle Informationen sichtbar sind. \n' +
-    'Hinweis: Relative Größen wie Prozentangaben, em, oder auch die named Schriftgrößen sind der richtige Weg. ';
+  @Input()
+  explanationText: string = '';
 
-  displayableText: string = this.explanationText;
+  displayableText: string = '';
 
+  @Input()
   maxLength: number = 200;
-  showToggleButton = true;
   isExpanded = true;
 
-  document: string = '.label-text{\n' +
-    'font-size: 16px;\n' +
-    '}\n' +
-    '\n' +
-    '.navbar-text{\n' +
-    'font-size: 16px; \n' +
-    '}\n';
+  @Input()
+  givenCode: string = '';
 
   constructor(private webshopCodeService: WebshopCodeService) {
   }
 
+  ngOnInit() {
+    this.displayableText = this.explanationText;
+  }
+
   ngAfterViewInit() {
     this.editor = new EditorView({
-      doc: this.document,
+      doc: this.givenCode,
       parent: document.getElementById('code-editor-container')!,
       extensions: [basicSetup,
         EditorView.theme({
@@ -71,6 +61,7 @@ export class CodeEditorComponent implements AfterViewInit {
 
   applyStyle() {
     this.webshopCodeService.updateCssText(this.editor.state.doc.text.toString().replace(/,/g, '\n'));
+    this.checkAnswer.emit(this.editor.state.doc.text);
   }
 
   toggleText(): void {
--- src/app/visual/font-size/font-size.component.ts
+++ src/app/visual/font-size/font-size.component.ts
@@ -1,18 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CodeEditorComponent } from '../../common/code-editor/code-editor.component';
 import { BrowserSimulationComponent } from '../../common/browser-simulation/browser-simulation.component';
 import { WebshopComponent } from '../../usecases/webshop/webshop.component';
+import { FormsModule } from '@angular/forms';
+import { BarrierData, simulationData } from '../../introduction/simulation/model/simulation-type-data';
 
 @Component({
   selector: 'app-font-size',
   imports: [
     CodeEditorComponent,
     BrowserSimulationComponent,
-    WebshopComponent
+    WebshopComponent,
+    FormsModule
   ],
   templateUrl: './font-size.component.html',
   styleUrl: './font-size.component.scss'
 })
-export class FontSizeComponent {
+export class FontSizeComponent implements OnInit{
 
+  noBarrierValue = {id: 'none', name: 'Keine Einschränkung', explanation: ''};
+  selectedSimulation?: BarrierData = this.noBarrierValue;
+  visualBarriersSimulation: BarrierData[] = [];
+  selectedTextSize: number = 0;
+  explanation: string = 'In dieser Lektion schauen wir uns den Stellenwert der Schriftgröße an. Auf der ' +
+    'Webseite ist der aus der Einführungslektion bekannte Webshop dargestellt, wie eine Person mit starker ' +
+    'Weitsichtigkeit ihn wahrnehmen würde. Die spezielle visuelle Einschränkung kannst du über den Dropdown-Button ' +
+    'anpassen. \n' +
+    'Versuch nun zunächst den hinzugefügten Text zu lesen. Ganz schön schwierig oder? Wie sieht es mit vergrößern aus ' +
+    '(Die Textgröße kannst du über den zusätzlichen Input verändern)? Die Webseite wurde so gebaut, dass automatisches ' +
+    'Vergrößern keine Auswirkungen auf die Schriftgröße hat. Das ist natürlich schlecht für Personen, die Schwierigkeiten haben, kleinere Texte wahrzunehmen.\n' +
+    '\n' +
+    'Versuch die CSS-Klasse so anzupassen, dass der Text beim Vergrößern lesbar ist und auch alle Informationen sichtbar sind. \n' +
+    'Hinweis: Relative Größen wie Prozentangaben, em, oder auch die named Schriftgrößen sind der richtige Weg. ';
+  codeExample = '.label-text{\n' +
+    'font-size: 16px;\n' +
+    '}\n' +
+    '\n' +
+    '.navbar-text{\n' +
+    'font-size: 16px; \n' +
+    '}\n';
+
+  ngOnInit() {
+    this.visualBarriersSimulation = [
+      this.noBarrierValue,
+      ...simulationData['visual'].barriers
+    ];
+    this.selectedSimulation = this.visualBarriersSimulation.find(el => el.id === 'farsighted');
+  }
+
+  setCorrectFontSize(style: Text): boolean {
+    //TODO Check all lines for fontsize & no px;
+    console.log(style);
+    return true;
+  }
 }
